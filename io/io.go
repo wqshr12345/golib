5bbcc435c8dec20e11b793a9ec702ed0d35392d3
fatedier <fatedier@gmail.com>
2023-03-11 15:37:13 +0800

    io.Join returns errors (#17)

--- io/io.go
+++ io/io.go
@@ -23,22 +23,29 @@ import (
 )
 
 // Join two io.ReadWriteCloser and do some operations.
-func Join(c1 io.ReadWriteCloser, c2 io.ReadWriteCloser) (inCount int64, outCount int64) {
+func Join(c1 io.ReadWriteCloser, c2 io.ReadWriteCloser) (inCount int64, outCount int64, errors []error) {
 	var wait sync.WaitGroup
-	pipe := func(to io.ReadWriteCloser, from io.ReadWriteCloser, count *int64) {
+	recordErrs := make([]error, 2)
+	pipe := func(number int, to io.ReadWriteCloser, from io.ReadWriteCloser, count *int64) {
 		defer to.Close()
 		defer from.Close()
 		defer wait.Done()
 
 		buf := pool.GetBuf(16 * 1024)
 		defer pool.PutBuf(buf)
-		*count, _ = io.CopyBuffer(to, from, buf)
+		*count, recordErrs[number] = io.CopyBuffer(to, from, buf)
 	}
 
 	wait.Add(2)
-	go pipe(c1, c2, &inCount)
-	go pipe(c2, c1, &outCount)
+	go pipe(0, c1, c2, &inCount)
+	go pipe(1, c2, c1, &outCount)
 	wait.Wait()
+
+	for _, e := range recordErrs {
+		if e != nil {
+			errors = append(errors, e)
+		}
+	}
 	return
 }
 
